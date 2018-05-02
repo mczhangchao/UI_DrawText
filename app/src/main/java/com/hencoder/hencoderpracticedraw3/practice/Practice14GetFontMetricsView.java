3b0e9721b4ad3c214efe0372cfe616acf5170b7b
zhangchaofeng <zhangchaofeng@interrcs.com>
2018-05-02 11:38:31 +0800

    Initial commit

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
@@ -0,0 +1,64 @@
+package com.hencoder.hencoderpracticedraw3.practice;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+public class Practice14GetFontMetricsView extends View {
+    Paint paint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+    String[] texts = {"A", "a", "J", "j", "Â", "â"};
+    int top = 200;
+    int bottom = 400;
+
+    public Practice14GetFontMetricsView(Context context) {
+        super(context);
+    }
+
+    public Practice14GetFontMetricsView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice14GetFontMetricsView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        paint1.setStyle(Paint.Style.STROKE);
+        paint1.setStrokeWidth(20);
+        paint1.setColor(Color.parseColor("#E91E63"));
+        paint2.setTextSize(160);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawRect(50, top, getWidth() - 50, bottom, paint1);
+
+        // 使用 Paint.getFontMetrics() 计算出文字的显示区域
+        // 然后计算出文字的绘制位置，从而让文字上下居中
+        // 这种居中算法的优点是，可以让不同的文字的 baseline 对齐
+
+        Paint.FontMetrics fontMetrics=paint2.getFontMetrics();
+        Log.d("Practice14 top is ",String.valueOf(fontMetrics.top));
+        Log.d("Practice14 bottom is ",String.valueOf(fontMetrics.bottom));
+        Log.d("Practice14 ascent is ",String.valueOf(fontMetrics.ascent));
+        Log.d("Practice14 descent is ",String.valueOf(fontMetrics.descent));
+        Log.d("Practice14 leading is ",String.valueOf(fontMetrics.leading));
+        float height=Math.abs(fontMetrics.ascent)+fontMetrics.descent;
+        int middle = bottom-top+Math.round(Math.abs(fontMetrics.ascent));
+        Log.d("Practice14 middle is ",String.valueOf(middle));
+        canvas.drawText(texts[0], 100, middle, paint2);
+        canvas.drawText(texts[1], 200, middle, paint2);
+        canvas.drawText(texts[2], 300, middle, paint2);
+        canvas.drawText(texts[3], 400, middle, paint2);
+        canvas.drawText(texts[4], 500, middle, paint2);
+        canvas.drawText(texts[5], 600, middle, paint2);
+    }
+}
